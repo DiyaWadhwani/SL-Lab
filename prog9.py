40510919d23d4d3080d39b593df034b3d16e36b1
DiyaWadhwani <54880903+DiyaWadhwani@users.noreply.github.com>
2019-09-13 10:21:22 +0530

    Update prog9.py

--- prog9.py
+++ prog9.py
@@ -5,3 +5,8 @@ def wordCounter(lyrics):
 		return Counter(f.read().split())
 		
 print("No. of words in the file :",wordCounter("highonlife.txt"))
+
+*****
+output:
+No. of words in the file : Counter({'day': 4, 'the': 4, 'die': 4, 'High': 3, "'til": 3, 'on': 3, 'we': 3, 'life': 3, 'see': 1, "I'd": 1, 'to': 1, 'I': 1, 'million': 1, 'walk': 1, 'And': 1, 'miles,': 1, "'Til": 1, 'just': 1, 'a': 1, 'you': 1, 'smile': 1})
+
