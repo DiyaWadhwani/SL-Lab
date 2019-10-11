1fb3637f2cce77696467481bdad253fa2efb4535
DiyaWadhwani <54880903+DiyaWadhwani@users.noreply.github.com>
2019-10-11 10:34:56 +0530

    Delete prog8.py

--- prog8.py
+++
@@ -1,7 +0,0 @@
-#list of even numbers and reversing them
-list1=input("Enter list of numbers :")
-list1=list1.split(' ')
-list2=[x for x in list1 if int(x) % 2 == 0]
-print("List of even numbers : ",list2)
-list2.reverse()
-print("Reversed list :",list2)
